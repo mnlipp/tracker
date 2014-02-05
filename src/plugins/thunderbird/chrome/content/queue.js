85ff43589c55e66ed35f1cb34b530ffb894c6948
Michael Lipp <mnl@mnl.de>
2014-02-05 18:30:05 +0100

    Trackerbird: improved item handling and reporting.

--- src/plugins/thunderbird/chrome/content/queue.js
+++ src/plugins/thunderbird/chrome/content/queue.js
@@ -1,17 +1,19 @@
 org.bustany.TrackerBird.Queue = function(delay) {
+	this._ui = org.bustany.TrackerBird.Ui;
+	this.__console = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
+	this._log = function(msg) {
+	   this.__console.logStringMessage(msg);
+	}
+
 	this._delay = delay;
 	this._items = [];
 	this._active = false;
 
 	var queue = this;
-	this._timerEvent = { notify: function(timer) { queue.process(); } };
+	this._timerEvent = { notify: function(timer) { queue._active = false; queue.process(); } };
 	this._queueTimer = null;
-}
-
-org.bustany.TrackerBird.Queue.prototype.__console = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
-
-org.bustany.TrackerBird.Queue.prototype._log = function(msg) {
-   this.__console.logStringMessage(msg);
+	dump(this._ui);
+	dump("Trackerbird created queue with delay " + delay + "\n");
 }
 
 org.bustany.TrackerBird.Queue.prototype.add = function(item) {
@@ -30,8 +32,10 @@ org.bustany.TrackerBird.Queue.prototype.process = function() {
 	}
 
 	if (this._items.length == 0) {
+		this._ui.showMessage("Idle");
 		return;
 	}
+	this._ui.showMessage(this._items.length + " items remaining");
 
 	this._active = true;
 
@@ -44,8 +48,6 @@ org.bustany.TrackerBird.Queue.prototype.process = function() {
 		dump ("Trackbird could not execute: " + ex + "\n");
 		this._log("Trackerbird could not execute: " + ex);
 	}
-
-	this._active = false;
 
 	this._queueTimer = Components.classes["@mozilla.org/timer;1"].createInstance(Components.interfaces.nsITimer);
 	this._queueTimer.initWithCallback(this._timerEvent, this._delay, Components.interfaces.nsITimer.TYPE_ONE_SHOT);
