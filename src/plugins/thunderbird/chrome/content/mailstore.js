c2dbe6979166d167843c63eb537d79284df153f3
Michael Lipp <mnl@mnl.de>
2014-02-06 10:51:01 +0100

    Trackerbird: fixed thunderbird "hangs".

--- src/plugins/thunderbird/chrome/content/mailstore.js
+++ src/plugins/thunderbird/chrome/content/mailstore.js
@@ -2,6 +2,40 @@ if (!org.bustany.TrackerBird.MailStore || !org.bustany.TrackerBird.MailStore.__i
 
 Components.utils.import("resource:///modules/MailUtils.js");
 
+org.bustany.TrackerBird.ContentRetriever = function(header, callback) {
+	this._header = header;
+	this._callback = callback;
+	this._contents = "";
+}
+
+org.bustany.TrackerBird.ContentRetriever.prototype = {
+	onStartRequest: function(request, ctx) {
+	},
+
+	onDataAvailable: function(request, context, inputStream, offset, count) {
+	    var scriptableInputStream = 
+	    	Components.classes["@mozilla.org/scriptableinputstream;1"].
+	    		createInstance(Components.interfaces.nsIScriptableInputStream);
+	    scriptableInputStream.init(inputStream);
+	    var data = scriptableInputStream.read(count);
+	    this._contents += data;
+	},
+
+	onStopRequest: function(request, ctx, status) {
+		// Basic html removing
+		this._contents = this._contents.replace(/<[^>]+?>/g, "");
+		this._callback(this._header, this._contents);
+	},
+	
+	QueryInterface: function (aIID) {
+		if (aIID.equals(Components.interfaces.nsIStreamListener) ||
+			aIID.equals(Components.interfaces.nsISupports)) {
+			return this;
+		}
+		throw Components.results.NS_NOINTERFACE;
+	}
+}
+	
 org.bustany.TrackerBird.MailStore = {
 	// Init barrier
 	__initialized: true,
@@ -15,7 +49,7 @@ org.bustany.TrackerBird.MailStore = {
 
 	_folderListener: {
 		OnItemAdded: function(parentItem, item) {
-			dump("Trackerbird: tracked item added\n");
+			dump("Trackerbird: new item to be tracked\n");
 			var store = org.bustany.TrackerBird.MailStore;
 			var hdr = item.QueryInterface(Components.interfaces.nsIMsgDBHdr);
 
@@ -26,7 +60,7 @@ org.bustany.TrackerBird.MailStore = {
 		},
 
 		OnItemRemoved: function(parentItem, item) {
-			dump("Trackerbird: tracked item removed\n");
+			dump("Trackerbird: item to be untracked\n");
 			var store = org.bustany.TrackerBird.MailStore;
 			var hdr = item.QueryInterface(Components.interfaces.nsIMsgDBHdr);
 
@@ -58,6 +92,7 @@ org.bustany.TrackerBird.MailStore = {
 	_queue: null,
 	_walkFolderCallback: null,
 	_indexMessageCallback: null,
+	_indexMessageContentsCallback: null,
 	_removeMessageCallback: null,
 
 	_prefs: null,
@@ -78,6 +113,9 @@ org.bustany.TrackerBird.MailStore = {
 		this._queue = new org.bustany.TrackerBird.Queue(this._prefs.getIntPref("indexDelay")),
 		this._walkFolderCallback = function(item) { store.walkFolder(item); }
 		this._indexMessageCallback = function(msg) { store.indexMessage(msg); }
+		this._indexMessageContentsCallback = function(msg, contents) {
+			store.indexMessageContents(msg, contents);
+		}
 		this._removeMessageCallback = function(msg) { store.removeMessage(msg); }
 
         MailUtils.discoverFolders();
@@ -132,15 +170,15 @@ org.bustany.TrackerBird.MailStore = {
 		knownUris = null;
 
 		while (enumerator.hasMoreElements()) {
-			var msg = enumerator.getNext().QueryInterface(Components.interfaces.nsIMsgDBHdr);
+			var hdr = enumerator.getNext().QueryInterface(Components.interfaces.nsIMsgDBHdr);
 
-			if (uriCache[folder.getUriForMsg(msg)]) {
+			if (uriCache[folder.getUriForMsg(hdr)]) {
 				continue;
 			}
 
 			this._queue.add({
 			                 callback: this._indexMessageCallback,
-			                 data: msg
+			                 data: hdr
 			                });
 		}
 
@@ -148,51 +186,49 @@ org.bustany.TrackerBird.MailStore = {
 		db = null;
 	},
 
-	indexMessage: function(msg) {
-		var msgContents = this.getMessageContents(msg);
-		if (this._trackerStore.storeMessage(msg, msgContents)) {
-			this._persistentStore.rememberMessage(msg);
+	/**
+	 * This method indexes a new message. In order to do this, it has to retrieve its contents
+	 * first. The contents is only available as a stream that has to be read first.
+	 * When the contents has been read, indexMessageContents is called and the actual
+	 * indexing happens.
+	 */
+	indexMessage: function(header) {
+		var folder = header.folder;
+
+		var messenger = Components.classes["@mozilla.org/messenger;1"].
+		                createInstance(Components.interfaces.nsIMessenger);
+		var uri = folder.getUriForMsg(header);
+		var msgService = messenger.messageServiceFromURI(uri);
+
+		// Streaming data into a nsScriptableInputStream and then reading from it here
+		// makes thunderbird hang sometimes, so continue asynchronously.
+		try {
+			var msgStream = new org.bustany.TrackerBird
+				.ContentRetriever(header, this._indexMessageContentsCallback);
+			msgService.streamMessage(uri, msgStream, null, null, true, null);
+		} catch (ex) {
+			dump("Trackerbird could not get contents of message " + ex + "\n");
+		}
+	},
+
+	/**
+	 * Index the message by inserting it into our local store of indexed messages and
+	 * into the tracker store.
+	 */
+	indexMessageContents: function(msg, contents) {
+		try {
+			if (this._trackerStore.storeMessage(msg, contents)) {
+				this._persistentStore.rememberMessage(msg);
+			}
+		} catch (ex) {
+			dump("Trackerbird failed to index message: " + ex + "\n");
+			this._log("Trackerbird failed to index message: " + ex);
 		}
 	},
 
 	removeMessage: function(msg) {
 		this._trackerStore.deleteMessage(msg);
 		this._persistentStore.forgetMessage(msg);
-	},
-
-	getMessageContents: function(header) {
-		var folder = header.folder;
-		var contents = "";
-
-		var messenger = Components.classes["@mozilla.org/messenger;1"].
-		                createInstance(Components.interfaces.nsIMessenger);
-		var uri = folder.getUriForMsg(header);
-		var msgService = messenger.messageServiceFromURI(uri);
-		var msgStream = Components.classes["@mozilla.org/network/sync-stream-listener;1"].
-		                createInstance();
-		var consumer = msgStream.QueryInterface(Components.interfaces.nsIInputStream);
-		var scriptInput = Components.classes["@mozilla.org/scriptableinputstream;1"].
-		                  createInstance();
-		var scriptInputStream = scriptInput.
-		                        QueryInterface(Components.interfaces.nsIScriptableInputStream);
-		scriptInputStream.init(msgStream);
-
-		try {
-			msgService.streamMessage(uri, msgStream, null, null, true, null);
-		} catch (e) {
-			dump("Could not get contents of message " + uri + "\n");
-			return null;
-		}
-
-		scriptInputStream.available();
-		while (scriptInputStream.available()) {
-			contents += scriptInputStream.read(1024);
-		}
-
-		// Basic html removing
-		contents = contents.replace(/<[^>]+?>/g, "");
-
-		return contents;
 	},
 
 	shutdown: function() {
